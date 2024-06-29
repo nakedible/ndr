a518b249f91548d397d7b7679109f7d75a59e2ef
Nuutti Kotivuori <naked@iki.fi>
2024-06-29 18:51:21 +0000

    Quick tweaks to roller

--- roller/index.js
+++ roller/index.js
@@ -13,6 +13,8 @@ const button = document.getElementById("rollem");
 
 const result = document.getElementById("result");
 
+const formatter = new Intl.NumberFormat(undefined, { signDisplay: "exceptZero" });
+
 Box.init().then(async (world) => {
     button.disabled = false;
 });
@@ -22,7 +24,7 @@ Box.onRollComplete = (results) => {
     const neg = results[1].value;
     const posClass = pos == 2 ? 'F' : pos == 12 ? 'C' : 'N';
     const negClass = neg == 2 ? 'F' : neg == 12 ? 'C' : 'N';
-    result.textContent = `Result: ${pos} – ${neg} = ${pos - neg} ${posClass}${negClass}`;
+    result.textContent = `Result: ${pos} – ${neg} = ${formatter.format(pos - neg)} ${posClass}${negClass}`;
 }
 
 // const colors = [
@@ -39,6 +41,7 @@ Box.onRollComplete = (results) => {
 // ];
 
 button.addEventListener("click", (e) => {
+    result.textContent = "Rolling...";
     Box.clear();
     Box.add("2d6", { themeColor: "#434343" });
     Box.add("2d6", { themeColor: "#F23005" });

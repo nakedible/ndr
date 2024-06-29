9cd74f53068545e41d3378c2b645c4e05aedc3bd
Nuutti Kotivuori <naked@iki.fi>
2024-06-29 17:57:03 +0000

    First try at roller

---
+++ roller/index.js
@@ -0,0 +1,45 @@
+import DiceBox from "https://unpkg.com/@3d-dice/dice-box@1.0.8/dist/dice-box.es.min.js";
+
+let Box = new DiceBox("#dice-box", {
+    assetPath: "assets/",
+    origin: "https://unpkg.com/@3d-dice/dice-box@1.0.8/dist/",
+    theme: "default",
+    themeColor: "#feea03",
+    offscreen: true,
+    scale: 6
+});
+
+const button = document.getElementById("rollem");
+
+const result = document.getElementById("result");
+
+Box.init().then(async (world) => {
+    button.disabled = false;
+});
+
+Box.onRollComplete = (results) => {
+    const pos = results[0].value;
+    const neg = results[1].value;
+    const posClass = pos == 2 ? 'F' : pos == 12 ? 'C' : 'N';
+    const negClass = neg == 2 ? 'F' : neg == 12 ? 'C' : 'N';
+    result.textContent = `Result: ${pos} – ${neg} = ${pos - neg} ${posClass}${negClass}`;
+}
+
+// const colors = [
+//     "#348888",
+//     "#22BABB",
+//     "#9EF8EE",
+//     "#FA7F08",
+//     "#F24405",
+//     "#F25EB0",
+//     "#B9BF04",
+//     "#F2B705",
+//     "#F27405",
+//     "#F23005"
+// ];
+
+button.addEventListener("click", (e) => {
+    Box.clear();
+    Box.add("2d6", { themeColor: "#434343" });
+    Box.add("2d6", { themeColor: "#F23005" });
+});
